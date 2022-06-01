93d3614bc3977b702d8b19b8973b20767f7f10f3
kollie-coder <apotiolasegun@gmail.com>
2022-05-31 22:18:33 -0700

    second commit

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const User = require("../models/User");
 const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
-const res = require("express/lib/response");
 
 //UPDATE
 router.put("/:id", async (req, res) => {
